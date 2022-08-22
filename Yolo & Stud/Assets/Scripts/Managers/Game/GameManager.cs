f0a1f8d432a009390b45261c6d393ef6daf8f5f6
LangaNhlanhla <digitalhumansworldwide@gmail.com>
2022-08-22 20:53:48 +0200

    Prefab Update + Enabling Build Mode through Inputs

--- Yolo & Stud/Assets/Scripts/Managers/Game/GameManager.cs
+++ Yolo & Stud/Assets/Scripts/Managers/Game/GameManager.cs
@@ -4,19 +4,52 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public class GameManager : Singleton<GameManager>
 {
-   
-    void Start()
+	#region Enums
+	public enum GameState { PlayMode, BuildMode, DesignMode}
+    public enum PlayerMode { IdleWalk, Studying, Resting, Customising}
+	#endregion
+
+	#region Variables
+	[Header("States & Enums")]
+	[SerializeField] GameState gameState;
+	[SerializeField] PlayerMode playerMode;
+
+	[Header("Unity Handles")]
+	[Tooltip("Drag The Tab Parent here")]
+	[SerializeField] GameObject tabParent;
+
+	[Header("Booleans")]
+	[Tooltip("This will determine if the tab parent will be on or off")]
+	[SerializeField] bool isTabParentOpen;
+	#endregion
+	void Start()
     {
-        
+        tabParent.SetActive(isTabParentOpen);
     }
 
    
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Tab))
+		{
+			isTabParentOpen = !isTabParentOpen;
+
+			//Change the game states
+			if (isTabParentOpen)
+				gameState = GameState.BuildMode;
+			else
+				gameState = GameState.PlayMode;
+
+			tabParent.SetActive(isTabParentOpen);
+			//Debug.Log(gameState);
+		}
     }
+
+	#region Public Functions Referenced
+	
+	#endregion
 }
 
 /*
