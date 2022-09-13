06d2901fc065c6b4744c013ac073ef9349f4f60c
LangaNhlanhla <digitalhumansworldwide@gmail.com>
2022-09-13 10:27:36 +0200

    Update lightControl.cs

--- Yolo & Stud/Assets/Scripts/UI/lightControl.cs
+++ Yolo & Stud/Assets/Scripts/UI/lightControl.cs
@@ -25,19 +25,26 @@ public class lightControl : MonoBehaviour
     {
         //lightIntensity.intensity  = intensitySlider.value;
 
-        var RSlider = RGBSliders[0].value;
-        var GSlider = RGBSliders[1].value;
-        var BSlider = RGBSliders[2].value;
+        var IntensityLight = RGBSliders[0].value;
+        var RSlider = RGBSliders[1].value;
+        var GSlider = RGBSliders[2].value;
+        var BSlider = RGBSliders[3].value;
 
         //First create a value
         var newColorRGB = new Color(RSlider, GSlider, BSlider);
-        //After change the colors
-        Lights[0].color = newColorRGB;
+        /*ights[0].color = newColorRGB;
         Lights[1].color = newColorRGB;
         Lights[2].color = newColorRGB;
         Lights[3].color = newColorRGB;
         Lights[4].color = newColorRGB;
         Lights[5].color = newColorRGB;
+        */
+
+		for (int i = 0; i < Lights.Length; i++)
+		{
+			Lights[i].intensity = IntensityLight;
+            Lights[i].color = newColorRGB;
+		}
 
     }
 }
