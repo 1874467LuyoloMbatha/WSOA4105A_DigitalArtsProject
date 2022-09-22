38320283c049510066ceaca1ae9d03916a3cfa37
LangaNhlanhla <digitalhumansworldwide@gmail.com>
2022-09-22 11:01:05 +0200

    Camera Set Up + Customising Cam Functional

--- Yolo & Stud/Assets/Scripts/Managers/Game/GameManager.cs
+++ Yolo & Stud/Assets/Scripts/Managers/Game/GameManager.cs
@@ -2,13 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using Cinemachine;
 using UnityEngine;
 
 public class GameManager : Singleton<GameManager>
 {
 	#region Enums
-	public enum GameState { PlayMode, BuildMode, DesignMode}
-    public enum PlayerMode { IdleWalk, Studying, Resting, Customising}
+	public enum GameState { PlayMode, BuildMode, PlayerCustomiserMode}
+    public enum PlayerMode { IdleWalk, Studying, Resting}
 	#endregion
 
 	#region Variables
@@ -19,15 +20,25 @@ public class GameManager : Singleton<GameManager>
 	[Header("Unity Handles")]
 	[Tooltip("Drag The Tab Parent here")]
 	[SerializeField] GameObject tabParent;
+	[Tooltip("Drag The Main Virtual Camera Here!")]
+	[SerializeField] CinemachineVirtualCamera mainVirtualCam;
+	[Tooltip("Drag The Customising Virtual Camera Here!")]
+	[SerializeField] CinemachineVirtualCamera customisingVirtualCamera;
 
 	[Header("Booleans")]
 	[Tooltip("This will determine if the tab parent will be on or off")]
 	[SerializeField] bool isTabParentOpen;
+	[Tooltip("This will determine if the player is Customising themeselves or not")]
+	[SerializeField] bool isCustomising;
 	#endregion
 	void Start()
     {
         tabParent.SetActive(isTabParentOpen);
-    }
+
+		//Set The Cameras
+		mainVirtualCam.Priority = 1;
+		customisingVirtualCamera.Priority = 0;
+	}
 
    
     void Update()
@@ -45,12 +56,41 @@ public class GameManager : Singleton<GameManager>
 			tabParent.SetActive(isTabParentOpen);
 			//Debug.Log(gameState);
 		}
+
+		
     }
 
 	#region Public Functions Referenced
 	public GameState GetGameState()
 	{
 		return gameState;
+	}
+
+	public void SwitchToCustomisingMode()
+	{
+		SetCustomising();
+
+		if(gameState == GameState.PlayerCustomiserMode)
+		{
+			mainVirtualCam.Priority = 0;
+			customisingVirtualCamera.Priority = 1;
+		}
+		else
+		{
+			mainVirtualCam.Priority = 1;
+			customisingVirtualCamera.Priority = 0;
+		}
+	}
+
+	public void SetCustomising()
+	{
+		isCustomising = !isCustomising;
+
+		if (isCustomising)
+			gameState = GameState.PlayerCustomiserMode;
+		else
+			gameState = GameState.PlayMode;
+
 	}
 	#endregion
 }
