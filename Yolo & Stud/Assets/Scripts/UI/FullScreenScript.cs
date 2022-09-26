6e105cf26470ae7fedf5309db53ed08ccb4ee26a
1874467LuyoloMbatha <81048286+1874467LuyoloMbatha@users.noreply.github.com>
2022-09-26 22:00:36 +0200

    minimise screen

---
+++ Yolo & Stud/Assets/Scripts/UI/FullScreenScript.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class FullScreenScript : MonoBehaviour
+{
+    public void FScreen( bool is_fullScreeen)
+    {
+        Screen.fullScreen = is_fullScreeen;
+
+        Debug.Log("FullScreen is " + is_fullScreeen);
+    }
+}
