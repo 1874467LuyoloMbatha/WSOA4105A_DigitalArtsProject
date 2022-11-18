51f6521e3042b59791778d0a6389086515b160bb
LangaNhlanhla <2119331@students.wits.ac.za>
2022-11-18 17:29:00 +0200

    Really Scrweing saving of genres, works sometimes

--- Yolo & Stud/Assets/Scripts/Audio/Sound Effects/soundsManager.cs
+++ Yolo & Stud/Assets/Scripts/Audio/Sound Effects/soundsManager.cs
@@ -54,8 +54,10 @@ public class soundsManager : Singleton<soundsManager>
     [SerializeField] string artistName;
     [Tooltip("Will be replaced with the song's name")]
     [SerializeField] string songName;
+	[Tooltip("Stores the current genre")]
+	[SerializeField] string genreStoragePrefs = "GenreIndex";
 
-    [Header("Music Mode Details")]
+	[Header("Music Mode Details")]
     [Tooltip("This string will appear if the player is using the music controller offline")]
     [SerializeField] string offlineMusicMode = "Playing Music Offline";
     [Tooltip("This string will appear if the player is using the music controller online")]
@@ -354,6 +356,7 @@ public class soundsManager : Singleton<soundsManager>
         }
         //Offline Music
 
+        SetUpGenreInAwake();
         songToSkip = offlineAudioSource.clip;
 
     }
@@ -364,7 +367,8 @@ public class soundsManager : Singleton<soundsManager>
     }
     private void Start()
     {
-        SetUpDropDownGenre();
+		SetUpGenreInAwake();
+		SetUpDropDownGenre();
         CheckMusicMode();
         UpdateAudioUi();
         PlayPauseAudio();
@@ -1055,6 +1059,14 @@ public class soundsManager : Singleton<soundsManager>
 
 	#region Offline Music Genre Logic
 
+    void SetUpGenreInAwake()
+    {
+        if(PlayerPrefs.HasKey(genreStoragePrefs))
+        {
+            genreIndex = PlayerPrefs.GetInt(genreStoragePrefs);
+			musicGenre = (MusicGenre)genreIndex;
+		}
+    }
     void SetUpDropDownGenre()
 	{
         genreIndex = ((int)musicGenre);
@@ -1079,6 +1091,13 @@ public class soundsManager : Singleton<soundsManager>
         genreIndex = v;
 
         musicGenre = (MusicGenre)genreIndex;
+
+		PlayerPrefs.SetInt(genreStoragePrefs, genreIndex);
+
+		if (musicGenre == MusicGenre.Custom)
+            trackIndex = 0;
+
+
 
         SortThroughGenre();
         
