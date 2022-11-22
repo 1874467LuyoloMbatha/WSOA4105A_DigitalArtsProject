e324461efcc6309df7147a45a08373c4f42171b1
LangaNhlanhla <2119331@students.wits.ac.za>
2022-11-22 12:09:44 +0200

    some saves before crashing

---
+++ Yolo & Stud/Assets/Scripts/Misc/SibuMatChange.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+[System.Serializable]
+public class SibuMatChange 
+{
+	public string bodyType;
+	public Material materialsToChange;
+}
+
+/*
+
+Copyright (C) Nhlanhla 'Stud' Langa
+
+*/
